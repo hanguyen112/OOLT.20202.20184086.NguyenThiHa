64ed60de1967def6dc0a5068df0b41d25d9b2166
hanguyen112 <79994104+hanguyen112@users.noreply.github.com>
2021-04-07 22:26:54 +0700

    Add files via upload

---
+++ Lab06/hust/soict/hedspi/aims/Media/Book.java
@@ -0,0 +1,22 @@
+package hust.soict.globalict.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+	private String title;
+	private String category;
+	private float cost;
+	private List<String> authors = new ArrayList<String>();
+	
+	public Book() {
+		//TODO Auto-generated constructor stub
+	}
+    public boolean addAuthor(String authorName) {
+    	
+    	
+    }
+    public boolean removeAuthor(String authorName) {
+    	
+    }
+}
